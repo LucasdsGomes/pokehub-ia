017f4efa1fd88aef0e0dc5d13d4f43505afd9f6f
Lucas Gomes <programmingpyrubygo@gmail.com>
2025-09-05 00:12:14 -0300

    creating assistant and game, with update in dashboard

---
+++ src/app/game/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function GamePage() {
+    return (
+        <div className="justify-center">
+            <p className="text-center">
+                Game
+            </p>
+        </div>
+    )
+}
